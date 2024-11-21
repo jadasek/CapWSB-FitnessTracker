6d502beed74bab6ae11636fc39a9cb9e652b0021
jadasek <55362267+jadasek@users.noreply.github.com>
2024-11-21 09:59:10 +0100

    Zad3

--- src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingRepository.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingRepository.java
@@ -11,10 +11,29 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface TrainingRepository extends JpaRepository<Training, Long> {
-    List<Training> findByUserId(Long userId); 
-    List<Training> findByEndTimeAfter(Date afterTime);
-    List<Training> findByActivityType(ActivityType activityType);
-    
-}
+public interface TrainingRepository extends JpaRepository<Training, Long> {
 
+    /**
+     * Retrieves a list of trainings associated with a specific user ID.
+     *
+     * @param userId the ID of the user whose trainings are to be retrieved
+     * @return a list of Trainings related to the specified user ID
+     */
+    List<Training> findByUserId(Long userId);
+
+    /**
+     * Retrieves a list of trainings that have an end time after the specified date.
+     *
+     * @param afterTime the date to compare against the end time of the trainings
+     * @return a list of Trainings that end after the specified time
+     */
+    List<Training> findByEndTimeAfter(Date afterTime);
+
+    /**
+     * Retrieves a list of trainings that are associated with a specific activity type.
+     *
+     * @param activityType the activity type to filter trainings by
+     * @return a list of Trainings associated with the specified activity type
+     */
+    List<Training> findByActivityType(ActivityType activityType);
+}
